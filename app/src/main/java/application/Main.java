cce896e9491675a97ad6ab24ac798f48b1f2911e
mauro roque <maurorodriguesroque@gmail.com>
2024-04-17 23:42:31 +0000

    comit

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,28 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+
+        int[] valor = new int[5];
+
+        for (int i = 0; i < valor.length; i++) {
+            Scanner leitor = new Scanner(System.in);
+            try {
+                valor[i] = leitor.nextInt();    
+            } catch (InputMismatchException error) {
+                System.out.println("Tipo invalido, definido no vetor" + error);
+                i--;
+            }
+        }
+
+        for (int i : valor) {
+            System.out.println(i);
+        }
+
     }
 }
 
